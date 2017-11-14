58efb0c39527b9f0466af06f1fb4a412c08ab24b
danielscake <daniel.ruginets@gmail.com>
2017-11-13 21:55:32 -0500

    service worker fix?

--- js/sw.js
+++ js/sw.js
@@ -1,8 +1,8 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-  '/',
-  '/styles/main.css',
-  '/script/main.js'
+  '/index.html',
+  '/css/main.css',
+  '/js/mainjava.js'
 ];
 
 self.addEventListener('install', function(event) {
